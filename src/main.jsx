43c0f5c529512b0d9a5bbc8b549d91d78e439ae8
Axel VandenHeuvel <axelvandenheuvel@Axels-MacBook-Pro.local>
2025-10-22 11:59:16 -0600

    Update package-lock.json and configure for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/axelvh-dev">
+    <BrowserRouter basename="/axeldev">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
